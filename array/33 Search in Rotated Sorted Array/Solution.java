5ace1fb9aa0c03dd4757410c2279dc0f0905c9e5
Zubin <gruhap.dev3@gmail.com>
2020-10-31 23:15:11 +0530

    Added #33

---
+++ array/33 Search in Rotated Sorted Array/Solution.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+
+    // Complete the function implementation below
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target) {
+                return mid;
+
+            } else if(nums[low] <= nums[mid]) {
+                if(nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if(nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int nums[] = new int[n];
+        for(int i = 0 ; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        int q = scanner.nextInt();
+        while(q > 0) {
+            int target = scanner.nextInt();
+            int result = new Solution().search(nums , target);
+            System.out.println(result);
+            q--;
+        }
+    }
+}
